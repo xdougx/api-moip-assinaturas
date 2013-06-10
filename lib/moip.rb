fa31d208d4d19d549a6d4dbd64ee4a8633e9a3d0
Douglas Rossignolli <douglas.rossignolli@gmail.com>
2013-06-10 16:43:44 -0300

    fix

--- lib/moip.rb
+++ lib/moip.rb
@@ -10,3 +10,5 @@ Dir["#{Dir.pwd}/lib/moip/**.rb"].each { |file| require file }
 
 # require all models
 Dir["#{Dir.pwd}/lib/moip/models/**"].each { |file| require file }
+
+Moip.configure {|config| config.token = "3JECRRW49I3HHKHI8VC517EDMS2SS1S9"; config.acount_key = "AZQDHSQ8MSL3F57JBERR59HKBVXVDMSDXQ8V1S6V"}
