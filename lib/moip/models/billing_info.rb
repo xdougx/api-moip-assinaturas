bf1cbc96c54a240354a05218661f44745678a015
Douglas Rossignolli <douglas.rossignolli@gmail.com>
2013-06-26 18:43:29 -0300

    added a validation to a Bill for validates card number
    pattens validates kind of cards where is invalid in some cases like:
    4444 5555 4444 6666,
    1111 8888 4444 5555,
    5555 6666 9999 7777

--- lib/moip/models/billing_info.rb
+++ lib/moip/models/billing_info.rb
@@ -7,7 +7,37 @@ class Moip::BillingInfo < Moip::Model
 	validates_format_of :expiration_month, :with => /[0-9]{2}/
 	validates_format_of :expiration_year, :with => /[0-9]{2}/
 	validates_format_of :number, :with => /[0-9]{16}/
+	
+	validate :valida_numero_cartao
 
+
+	def valida_numero_cartao
+		patterns.each do |pattern|
+			self.errors.add :number, "is invalid" and return if @number.to_s.match pattern
+			false
+		end 
+	end
+
+	# pattens validates kind of cards where is invalid in some cases like:
+	# E.g.: 4444 5555 4444 6666, 1111 8888 4444 5555, 5555 6666 9999 7777 
+	# has 10000 combinations
+	def patterns
+		@@patterns ||= []
+
+		if @@patterns.size == 0
+			for i in 0..9
+				for j in 0..9
+					for k in 0..9
+						for l in 0..9
+							@@patterns << Regexp.new("[#{i}]{4}[#{j}]{4}[#{k}]{4}[#{l}]{4}")
+						end
+					end
+				end
+			end
+		end
+
+		@@patterns
+	end
 
 	def attributes
 		{
