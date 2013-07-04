e6f6d477d61c22f92f3c2227e7c27006e6622b17
Douglas Rossignolli <douglas.rossignolli@gmail.com>
2013-07-04 16:32:58 -0300

    Revert "fix"
    This reverts commit 5392df77e708836380c2ea3c4ea0f048cc08e1bc.

--- lib/moip/model.rb
+++ lib/moip/model.rb
@@ -1,10 +1,9 @@
 # encoding: utf-8
 module Moip
 	class Model		
-		extend ActiveModel::Errors
 		include ActiveModel::Validations
 		include ActiveModel::Serialization
-
+		include ActiveModel::Errors
 
 		def to_json
 			hash = self.serializable_hash.delete_if {|key, value| value.nil? }
